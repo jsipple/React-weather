51f2e3c0f309d879fa44ef49b4081e0ca771b75b
unknown <sipple3@gmail.com>
2019-02-22 15:40:50 -0500

    made icons change based on whether or not the sun has set in the location

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -21,7 +21,9 @@ class Weather extends Component {
             input: '',
             lat: '',
             long: '',
-            weather: ''
+            weather: '',
+            sunrise: '',
+            sunset: ''
         }
         
     }
@@ -38,16 +40,12 @@ class Weather extends Component {
       function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
-      } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-      }
+      } 
     }
     getLocation()
     // the this needs to be bound to the outside so that i can setState and run next function based on that
 //   above fixed :)
     function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude; 
     lat = position.coords.latitude
     long = position.coords.longitude
     that.setState({
@@ -62,54 +60,70 @@ componentDidUpdate = (prevProps, prevState) => {
     if (this.state.long !== prevState.long) {
         this.apiByLatLong()
     }
-    // might edit this so that it grabs the time to determine if day or night
+    let d = new Date().getTime()/1000
+    let nd;
+    // the latitude one does not work for this so it says always night
+    if (d > this.state.sunrise && d < this.state.sunset) {
+      nd = "d"
+    } else {
+      nd = "n"
+    }
+    // might edit this so that it grabs the time to determine if day or night can grab sunrise and sunset from api and convert current time
     let icon = document.getElementById("weatherIcon")
     let query;
+    let str = this.state.weather.toString()
     let regex1 = /50\d{1}/
     let regex2 = /2\d{2}/
     let regex3 = /3\d{2}/
     let regex4 = /6\d{2}/
     let regex5 = /7\d{2}/
-    let regex6 = /7\d{2}/
+    let regex6 = /8\d{2}/
     let regex7 = /51\d{1}/
     let regex8 = /52\d{1}/
     let url = "http://openweathermap.org/img/w/"
     if (this.state.weather !== prevState.weather) {
       if (regex1.test(this.state.weather)) {
-        query = "10d"
+        query = "10"
       }
       if (regex2.test(this.state.weather)) {
-        query = "11d"
+        query = "11"
       }
       if (regex3.test(this.state.weather)) {
-        query = "9d"
+        query = "9"
       }
       if (regex4.test(this.state.weather)) {
-        query = "13d"
+        query = "13"
       }
       if (regex5.test(this.state.weather)) {
-        query = "50d"
+        query = "50"
       }
       if (regex6.test(this.state.weather)) {
         if (this.state.weather === 804) {
-          let lastChar = this.state.weather.substr(-1)
-          query = "0" + lastChar + "d"
+          let lastChar = str.substr(-1)
+          query = "0" + lastChar
         } else {
-          let lastChar = this.state.weather.substr(-1)
+          let lastChar = str.substr(-1)
           lastChar = parseInt(lastChar)
           lastChar++
-          query = "0" + lastChar + "d"
+          query = "0" + lastChar
         }
       }
       if (regex7.test(this.state.weather)) {
-        query = "13d"
+        query = "13"
       }
       if (regex8.test(this.state.weather)) {
-        query = "09d"
+        query = "09"
       }
-      let queryURL = url + query + ".png"
+      let queryURL = url + query + nd + ".png"
       icon.src = queryURL
     }
+  if (this.state.units !== prevState.units) {
+    if (this.state.zipcode !== '') {
+      this.apiByZip()
+    } else {
+      this.apiByLatLong()
+    }
+  }
 }
   apiByLatLong = async () => {
     let apiKey = 'f1fa8d030881f674696a9ba2c10ca999'
@@ -123,10 +137,12 @@ componentDidUpdate = (prevProps, prevState) => {
     (result) => {
         this.setState({
             temp: result.list[0].main.temp,
-            weather: result.list[0].weather[0].id
+            weather: result.list[0].weather[0].id,
+            sunrise: result.list[0].sys.sunrise,
+            sunset: result.list[0].sys.sunset
         })
         // always getting the same result
-        console.log(result.list[0].weather[0].id)
+        console.log(result.list[0])
         console.log(this.state)
     },
     // Note: it's important to handle errors here
@@ -150,7 +166,9 @@ componentDidUpdate = (prevProps, prevState) => {
     (result) => {
         this.setState({
             temp: result.main.temp,
-            weather: result.list[0].weather[0].id
+            weather: result.weather[0].id,
+            sunrise: result.sys.sunrise,
+            sunset: result.sys.sunset
         })
         // always getting the same result
         console.log(result)
@@ -232,8 +250,6 @@ componentDidUpdate = (prevProps, prevState) => {
                     <img id="weatherIcon" src={src}/>
                     <br />
                     {this.state.temp}
-                    <br />
-                    {this.state.weather}
                 </form>
             </Fragment>
         )
