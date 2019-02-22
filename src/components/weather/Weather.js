278b14320bf9935a148ccf0a4074ca7bc3530d1c
Jacob Sipple <sipple3@gmail.com>
2019-02-21 20:29:04 -0500

    added functionality to grab the users current location

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,22 +1,91 @@
 import React, { Component,Fragment } from 'react';
 import {isoCountries} from './countries'
-
+let lat;
+let long;
+let that;
 class Weather extends Component {
     constructor(props) {
         super(props)
+        that=this
         this.state = {
-            zipcode: "27518",
-            country: 'US',
+            zipcode: "",
+            country: '',
             units: 'imperial',
             // want to grab the weather for the upcoming week
             temp: [],
-            input: ''
+            input: '',
+            lat: '',
+            long: ''
         }
+        
     }
     componentDidMount = () => {
-        this.api()
+        this.getLoc()
+
+
+// need to seach first time with latitude/longitude so maybe make a seperate function for the 
+// first api call or find out how to grab zip code/country from geolocator
+
   }
-  api =async () => {
+  getLoc = async () => {
+    var x = document.getElementById("demo");
+      function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+      }
+    }
+    getLocation()
+    //   api call running before this one if i await this says undefined
+    // the this needs to be bound to the outside so that i can setState and run next function based on that
+  function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude; 
+    lat = position.coords.latitude.toFixed(1)
+    long = position.coords.longitude.toFixed(1)
+    that.setState({
+        lat: lat,
+        long: long
+    })
+    console.log(lat)
+    console.log(long)
+  }
+}
+// basically just calling whenever long changes not working calling whenever any state changes
+// you do need to have prevProps
+componentDidUpdate = (prevProps, prevState) => {
+    if (this.state.long !== prevState.long) {
+        this.apiByLatLong()
+    }
+}
+  apiByLatLong = async () => {
+    let apiKey = 'f1fa8d030881f674696a9ba2c10ca999'
+    // might have to shorten latitude or long need to check on that
+//   let url = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID='
+  let queryURL = new Request('http://api.openweathermap.org/data/2.5/find?lat=' + lat  + "&lon=" + long + "&units=" + this.state.units + "&cnt=1&appid=" + apiKey)
+  console.log(queryURL)
+  fetch(queryURL)
+  .then(res => res.json())
+  .then(
+    (result) => {
+        this.setState({
+            temp: result.list[0].main.temp,
+            weather: result.list[0].weather[0].description
+        })
+        // always getting the same result
+        console.log(result)
+        console.log(this.state)
+    },
+    // Note: it's important to handle errors here
+    // instead of a catch() block so that we don't swallow
+    // exceptions from actual bugs in components.
+    (error) => {
+        console.log(error)
+    }
+  )
+}
+  apiByZip = async () => {
     await this.countrySelected()
     await this.unitSelected()
   let apiKey = 'f1fa8d030881f674696a9ba2c10ca999'
@@ -61,7 +130,7 @@ class Weather extends Component {
     this.unitSelected()
     )
     // call a different component
-    this.api()
+    this.apiByZip()
   }
   countrySelected = () => {
       let selects = document.getElementById("countries")
